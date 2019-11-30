41c84d1d342cb2132934e5309807a874fd7ce440
arahansa <arahansa@naver.com>
2019-12-01 01:17:06 +0900

    add ReamMe for jUnit 설명

--- first_spring_boot/learn-spring-boot-chap02-re/README.md
+++ first_spring_boot/learn-spring-boot-chap02-re/README.md
@@ -8,6 +8,12 @@ Add as Modules to 에서 루트 프로젝트가 안 보인다면 인텔리제이
 # 50 p
 server.port:80 은 yml 형식에서이고
 server.port=80 은 properties 파일에서 적어줘야합니다.
+
+# 55 p 
+
+https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-2.1-Release-Notes#junit-5
+https://reflectoring.io/spring-boot-test/
+에 의하면 Spring boot 2.1 버젼부터 SpringBootTest 에서 ExtendWith 를 제공하므로 JUnit5를 사용하면 굳이 적을 필요가 없다고합니다.
  
 # 57p
 @Data 라는 어노테이션이 있는 프로젝트 최초 설정시 lombok 라이브러리 설치와
--- first_spring_boot/learn-spring-boot-chap02-re/src/main/java/com/arahansa/learnspringbootchap02/controller/TestController.java
+++ first_spring_boot/learn-spring-boot-chap02-re/src/main/java/com/arahansa/learnspringbootchap02/controller/TestController.java
@@ -4,8 +4,12 @@ import com.arahansa.learnspringbootchap02.pojo.FruitProperty;
 import com.arahansa.learnspringbootchap02.pojo.FruitPropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -13,6 +17,13 @@ public class TestController {
     private String propertyTestName;
     @Autowired
     FruitProperty fruitProperty;
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @GetMapping("/bean-list")
+    public List<String> beanList(){
+        return Arrays.asList(applicationContext.getBeanDefinitionNames());
+    }
 
     @GetMapping("hi")
     public String helloWorld33(){
