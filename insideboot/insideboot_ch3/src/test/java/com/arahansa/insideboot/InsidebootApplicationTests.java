ad7909ec0b10c2347b553a43234c0517b6605c54
arahansa <arahansa@naver.com>
2019-12-01 16:27:45 +0900

    add test

--- insideboot/insideboot_ch3/src/test/java/com/arahansa/insideboot/InsidebootApplicationTests.java
+++ insideboot/insideboot_ch3/src/test/java/com/arahansa/insideboot/InsidebootApplicationTests.java
@@ -1,13 +1,22 @@
 package com.arahansa.insideboot;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class InsidebootApplicationTests {
 
+    @Autowired
+    SqlSessionTemplate sqlSession;
+
     @Test
-    void contextLoads() {
+    void testSqlSessioin(){
+        System.out.println(sqlSession.toString());
     }
 
 }
